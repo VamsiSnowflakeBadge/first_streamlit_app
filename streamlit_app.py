ee73ca8f7c29ef2f3bcb055776cd766f367b3e90
VamsiSnowflakeBadge <130387192+VamsiSnowflakeBadge@users.noreply.github.com>
2023-04-10 12:25:49 -0400

    Update streamlit_app.py
    added symbols

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,8 @@
 import streamlit
-streamlit.title('My Parents New Healthy Life Style')
-streamlit.header('Breakfast Menu')
-streamlit.text('Idly & Puri with curries and chutney')
-streamlit.text('Spinach and Beetroot')
+streamlit.title('🥣 My Parents New Healthy Life Style')
+streamlit.header('🥗 Breakfast Menu')
+streamlit.text('🐔 🍞Idly & Puri with curries and chutney')
+streamlit.text('🥗  Spinach and Beetroot')
 streamlit.text('Yoga and walk every day for 45 mins')
+streamlit.text('🥑Avacado Toast')
 
