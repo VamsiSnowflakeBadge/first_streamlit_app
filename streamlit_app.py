fe837e2e028160ad2b2b86536877f87d50adb400
VamsiSnowflakeBadge <130387192+VamsiSnowflakeBadge@users.noreply.github.com>
2023-04-10 12:22:51 -0400

    Update streamlit_app.py
    added more details

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,7 @@
 import streamlit
 streamlit.title('My Parents New Healthy Life Style')
+streamlit.header('Breakfast Menu')
+streamlit.text('Idly & Puri with curries and chutney')
+streamlit.text('Spinach and Beetroot')
+streamlit.text('Yoga and walk every day for 45 mins')
+
