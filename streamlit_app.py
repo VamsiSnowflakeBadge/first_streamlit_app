f93de3597a06d5dce4015816788b51e29b47a575
VamsiSnowflakeBadge <130387192+VamsiSnowflakeBadge@users.noreply.github.com>
2023-04-11 10:16:14 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,6 +7,8 @@ streamlit.text('Yoga and walk every day for 45 mins')
 streamlit.text('🥑Avacado Toast')
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
+
+import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 streamlit.dataframe(my_fruit_list)
 
