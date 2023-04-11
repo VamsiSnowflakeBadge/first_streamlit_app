636c09fb80bf6769d5af3ce050a0911f750c1b37
VamsiSnowflakeBadge <130387192+VamsiSnowflakeBadge@users.noreply.github.com>
2023-04-10 23:15:22 -0400

    Update streamlit_app.py
    delete

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,10 +18,6 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 
 
-import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/Avocado")
-streamlit.text(fruityvice_response)
-
 streamlit.header("Fruityvice Fruit Advice!")
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/Orange")
